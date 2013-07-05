fba808b80b9defad0b3427f0489a4179f0ac8fff
hiraken <htb@mvf.biglobe.ne.jp>
2013-07-06 04:21:32 +0900

    Initial Commit

---
+++ Video/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Video
+//
+//  Created by HIRAMATSU KENJIRO on 2013/07/06.
+//  Copyright (c) 2013年 HIRAMATSU KENJIRO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
